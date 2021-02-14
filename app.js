1d87d8f540280f735ef4d9867fc2f5c42e36f46d
Rakibul615 <rakibul01072019@gmail.com>
2021-02-14 23:08:46 +0600

    almost done

--- app.js
+++ app.js
@@ -1,25 +1,39 @@
-const searchSongs= () =>{
-    const searchText=document.getElementById('search-field').value;
-    const url=`https://api.lyrics.ovh/suggest/:${searchText}`;
+const searchSongs = () => {
+    const searchText = document.getElementById('search-field').value;
+    const url = `https://api.lyrics.ovh/suggest/:${searchText}`;
     fetch(url)
-    .then(res=>res.json())
-    .then(data=>displaySong(data.data))
+        .then(res => res.json())
+        .then(data => displaySong(data.data))
 }
-const displaySong=songs=>{
-    console.log(songs);
-    const songList= document.getElementById('song-list');
-    songs.forEach(element => {
-        const songDiv=document.createElement('div');
-        songDiv.className='single-result row align-items-center my-3 p-3';
-        songDiv.innerHTML=` <div class="col-md-9">
-        <h3 class="lyrics-name">${element.title}</h3>
-        <p class="author lead">Album by <span>${element.artist.name}</span></p>
+const displaySong = songs => {
+    const songList = document.getElementById('song-list');
+    songList.innerHTML="";
+    songs.forEach(song => {
+        const songDiv = document.createElement('div');
+        songDiv.className = 'single-result row align-items-center my-3 p-3';
+        songDiv.innerHTML = ` <div class="col-md-9">
+        <h3 class="lyrics-name">${song.title}</h3>
+        <p class="author lead">Album by <span>${song.artist.name}</span></p>
+        <audio controls>
+            <source src="${song.preview}" type="audio/ogg">
+        </audio>
     </div>
     <div class="col-md-3 text-md-right text-center">
-        <button class="btn btn-success">Get Lyrics</button>
+        <button onclick="getLyric('${song.artist.name}','${song.title}')" class="btn btn-success">Get Lyrics</button>
     </div>`;
         songList.appendChild(songDiv);
-        
+
     });
 
+}
+const getLyric=(artist,title)=>{
+    const url=` https://api.lyrics.ovh/v1/${artist}/${title}`;
+    //console.log(url);
+    fetch(url)
+ .then(res=>res.json())
+    .then(data=>displayLyric(data.lyrics))
+}
+const displayLyric=lyric=>{
+    const lyricDiv=document.getElementById('song-lyric');
+    lyricDiv.innerText=lyric;
 }
