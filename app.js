5d0c3ff5e4fdd0520bb5925ad3b3bb75a1565774
Rakibul615 <rakibul01072019@gmail.com>
2021-02-14 20:32:38 +0600

    something done

--- app.js
+++ app.js
@@ -5,8 +5,21 @@ const searchSongs= () =>{
     .then(res=>res.json())
     .then(data=>displaySong(data.data))
 }
-const displaySong= data=>{
-    data.forEach(data => {console.log(data.title)
+const displaySong=songs=>{
+    console.log(songs);
+    const songList= document.getElementById('song-list');
+    songs.forEach(element => {
+        const songDiv=document.createElement('div');
+        songDiv.className='single-result row align-items-center my-3 p-3';
+        songDiv.innerHTML=` <div class="col-md-9">
+        <h3 class="lyrics-name">${element.title}</h3>
+        <p class="author lead">Album by <span>${element.artist.name}</span></p>
+    </div>
+    <div class="col-md-3 text-md-right text-center">
+        <button class="btn btn-success">Get Lyrics</button>
+    </div>`;
+        songList.appendChild(songDiv);
         
     });
+
 }
